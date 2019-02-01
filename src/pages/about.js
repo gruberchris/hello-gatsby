bbcba4cd7c852a01ac14c60dc82b4012160d51fe
Chris Gruber <chris@chrisgruber.com>
2019-01-31 21:18:11 -0600

    Initial commit

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/header";
+import Layout from "../components/layout";
+
+export default () => (
+  <Layout>
+    <div style={{ color: `teal` }}>
+      <Header>About Gatsby, Yeah Baby!</Header>
+      <p>Such wow. Very React.</p>
+    </div>
+  </Layout>
+);
